b9473fff0c6ce20ced5f7954982fe1fdb5ffc216
Sejuty <70710399+Sejuty@users.noreply.github.com>
2022-04-01 16:43:01 +0600

    active search result and refurbishing of existing code

--- app.js
+++ app.js
@@ -2,16 +2,42 @@ let weather = {
   apiKey: "1e18dd0ef375591c26f27198d3cd3d3f",
   fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q="
-      +city
-      +"&units=metric&appid="
-      +this.apiKey
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log(this.displayWeather(data)));
   },
-  displayWeather: function () {
-    //this.fetchWeather();
+  displayWeather: function (data) {
+    const { name } = data;
+    const { description, icon } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    console.log(name, description, icon, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText ="Humidity: "+ humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: "+ speed + "m/s";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+"')";
+  },
+
+  search: function () {
+    this.fetchWeather(document.querySelector(".search_bar").value);
   }
-      
 };
+
+document.querySelector(".search_button").addEventListener("click", () => {
+    weather.search();
+});  
+document.querySelector(".search_bar").addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        weather.search();
+    }
+});
+weather.fetchWeather("Sylhet");
