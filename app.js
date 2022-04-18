09fc05d59157044e265065a95e9c6b6e710fbcfd
Sejuty <70710399+Sejuty@users.noreply.github.com>
2022-04-18 23:22:14 +0600

    error message updated

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ let weather = {
         "&units=metric&appid=" +
         this.apiKey
     )
-      .then((response) => response.json())
-      .then((data) => console.log(this.displayWeather(data)));
+      .then((response) => {
+        if(response.status === 200) {
+        response.json().then((data) => console.log(this.displayWeather(data)));
+        }
+        else if(response.status === 404) {
+          alert("City not found");
+        }
+      });
   },
   displayWeather: function (data) {
     const { name } = data;
@@ -20,24 +26,27 @@ let weather = {
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText ="Humidity: "+ humidity + "%";
-    document.querySelector(".wind").innerText = "Wind speed: "+ speed + "m/s";
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "m/s";
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+"')";
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
 
   search: function () {
     this.fetchWeather(document.querySelector(".search_bar").value);
-  }
+  },
 };
 
 document.querySelector(".search_button").addEventListener("click", () => {
-    weather.search();
-});  
+  weather.search();
+});
 document.querySelector(".search_bar").addEventListener("keyup", (e) => {
-    if (e.keyCode === 13) {
-        weather.search();
-    }
+  if (e.keyCode === 13) {
+    weather.search();
+  }
 });
 weather.fetchWeather("Sylhet");
